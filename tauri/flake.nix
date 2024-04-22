0245d21464b56418b4fd930caa656e231411a33d
Alfrheim <1399702+Alfrheim@users.noreply.github.com>
2024-04-22 14:38:08 +0200

    adding tauri template

---
+++ tauri/flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      # pkgs = nixpkgs.legacyPackages.${system};
+
+      libraries = with pkgs; [
+        webkitgtk
+        webkitgtk_4_1
+        gtk3
+        cairo
+        gdk-pixbuf
+        glib
+        dbus
+        openssl_3
+        librsvg
+        libayatana-appindicator
+      ];
+
+      packages = with pkgs; [
+        curl
+        wget
+        pkg-config
+        dbus
+        openssl_3
+        glib
+        gtk3
+        libsoup
+        webkitgtk
+        webkitgtk_4_1
+        librsvg
+        tor
+        corepack_21
+        libappindicator-gtk3
+        rust-bin.beta.latest.default
+        rust-analyzer
+      ];
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = packages;
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+          export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+        '';
+      };
+    });
+}
