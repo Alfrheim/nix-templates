58c748ae49c17b6dace03351cc90c91b6b814d52
Alfrheim <1399702+Alfrheim@users.noreply.github.com>
2025-02-04 09:15:51 +0100

    adding languages to template

--- flake.nix
+++ flake.nix
@@ -3,13 +3,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { self, nixpkgs, templates }: {
+  outputs = {
+    self,
+    nixpkgs,
+    templates,
+  }: {
     templates = {
       rust = {
         path = ./rust;
         description = "starter for rust";
       };
+      clojure = {
+        path = ./clojure;
+        description = "starter for clojure";
+      };
+      zig = {
+        path = ./zig;
+        description = "starter for zig";
+      };
+      java = {
+        path = ./java;
+        description = "starter for java";
+      };
+      tauri = {
+        path = ./tauri;
+        description = "starter for tauri";
+      };
     };
   };
 }
-
